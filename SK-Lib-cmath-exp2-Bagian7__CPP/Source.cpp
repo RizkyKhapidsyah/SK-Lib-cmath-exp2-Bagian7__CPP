b42c16b90c966656d63c0875005a1b200c4066de
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-24 16:32:38 +0700

    Add project files.

---
+++ SK-Lib-cmath-exp2-Bagian7__CPP/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019 */
+
+using namespace std;
+
+int main() {
+	long int x = 14;
+	double result;
+
+	result = exp2(x);
+	cout << "exp2(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
